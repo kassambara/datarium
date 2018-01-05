ed843e4dbac1eccaddaa1d69192719ee7a8f1155
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2018-01-05 09:02:34 +0100

    titanic and housetasks data added

---
+++ R/housetasks.raw.R
@@ -0,0 +1,12 @@
+#'Housetasks
+#'
+#'@description A data frame containing the frequency of execution of 13 house tasks in the couple.
+#'@name housetasks.raw
+#'@docType data
+#'@usage data("housetasks.raw")
+#'@format A data frame with 1744 rows and 2 columns (tasks and status).
+#' @examples
+#' data(housetasks.raw)
+#' table(housetasks.raw)
+#'
+NULL
---
+++ R/titanic.raw.R
@@ -0,0 +1,16 @@
+#'Survival of Passengers on the Titanic
+#'
+#'@description Survival of passengers on the Titanic. This data set provides
+#'  information on the fate of passengers on the fatal maiden voyage of the
+#'  ocean liner ‘Titanic’. Columns are economic status (Class), Sex,
+#'  Age and Survivived.
+#'@name titanic.raw
+#'@docType data
+#'@usage data("titanic.raw")
+#'@format A data frame with 2201 rows and 4 columns.
+#' @examples
+#' data(titanic.raw)
+#' data("titanic.raw")
+#' with(titanic.raw, table(Class, Survived))
+#'
+NULL
