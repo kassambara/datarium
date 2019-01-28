49b96c02e74f3822409cf49190e2d2ae3e53ade9
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-01-29 00:14:40 +0100

    depression data added for repeated measure anova tests

---
+++ R/depression.R
@@ -0,0 +1,19 @@
+#'Depression Data
+#'
+#'@description Demo dataset for running a repeated measures ANOVA.
+#'  The data correspond to an experiment in which a treatment for depression is
+#'  studied. Two groups of patients (1: control / 2: treatment) have been
+#'  followed at five different times (0: pre-test, 1: one month post-test, 3: 3
+#'  months follow-up and 6: 6 months follow-up). The dependant variable is a
+#'  depression score.
+#'
+#'  Repeated measures ANOVA can be performed in order to determine the effect
+#'  of the treatment and the effect of time on the depression score.
+#'@name depression
+#'@docType data
+#'@usage data("depression")
+#'@format A data frame with 24 rows and 5 columns.
+#' @examples
+#' data(depression)
+#' head(depression)
+NULL
---
+++ R/depression2.R
@@ -0,0 +1,19 @@
+#'Depression Data 2
+#'
+#'@description Demo dataset for repeated measures ANOVA using the mixed models.
+#'  The data correspond to an experiment in which a treatment for depression is
+#'  studied. Two groups of patients (1: control / 2: treatment) have been
+#'  followed at five different times (0: pre-test, 1: one month post-test, 3: 3
+#'  months follow-up and 6: 6 months follow-up). The dependant variable is a
+#'  depression score.
+#'
+#'  Repeated measures ANOVA can be performed in order to determine the effect
+#'  of the treatment and the effect of time on the depression score.
+#'@name depression2
+#'@docType data
+#'@usage data("depression2")
+#'@format A data frame with 96 rows and 4 columns.
+#' @examples
+#' data(depression2)
+#' head(depression2)
+NULL
