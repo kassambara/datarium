f1f2c929fe2e69b97a25ab88f4470e7c647ba8d6
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-01-30 00:08:44 +0100

    performance data added

---
+++ R/performance.R
@@ -0,0 +1,13 @@
+#'Performance Data for Three-Way Mixed ANOVA
+#'
+#'@description Contains the performance score measures of participants at two
+#'  time points. The aim of this study is to evaluate the effect of gender and
+#'  anxiety on performance score. The three-way mixed ANOVA test can be used.
+#'@name performance
+#'@docType data
+#'@usage data("performance")
+#'@format A data frame with 24 rows and 5 columns.
+#' @examples
+#' data(performance)
+#' head(performance)
+NULL
