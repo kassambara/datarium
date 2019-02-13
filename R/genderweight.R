da2eb01041e228592a698aa966bb395887107d19
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-02-13 23:41:49 +0100

    genderweight data added

---
+++ R/genderweight.R
@@ -0,0 +1,15 @@
+#'Weight Data By Gender for Two-Samples Mean Test
+#'
+#'@description Contains the weights by sex (M for male; F for
+#'  female). The question is whether the average women’s weight differs from the
+#'  average men’s weight?
+#'
+#'  A two-samples independent t-test can be performed to answer to this question.
+#'@name genderweight
+#'@docType data
+#'@usage data("genderweight")
+#'@format A data frame with 40 rows and 2 columns
+#' @examples
+#' data(genderweight)
+#' head(genderweight)
+NULL
