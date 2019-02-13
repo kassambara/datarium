e8156bc8f97d125c21d54c8dfe5ab21d31459b9e
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-02-13 23:03:17 +0100

    mice data added

---
+++ R/mice.R
@@ -0,0 +1,14 @@
+#'Mice Weight Data for One Sample Mean Test
+#'
+#'@description Contains the weight of 10 mice. The question is whether the
+#'  average weight of the mice differs from 25g.
+#'
+#' A one sample t-test can be performed to answer to this question.
+#'@name mice
+#'@docType data
+#'@usage data("mice")
+#'@format A data frame with 10 rows and 2 columns
+#' @examples
+#' data(mice)
+#' head(mice)
+NULL
