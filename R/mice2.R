c5aeb93d42eba02795a1637f0137934ecbd8d15b
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-02-14 00:31:18 +0100

    mice2 data added for paired samples t-test

---
+++ R/mice2.R
@@ -0,0 +1,13 @@
+#' Mice Weight Data for Paired-Samples Mean Test
+#'
+#'@description contains the weight of 10 mice before and after the treatment.
+#'
+#' A paired-samples t-test can be performed to answer to this question.
+#'@name mice2
+#'@docType data
+#'@usage data("mice2")
+#'@format A data frame with 10 rows and 3 columns
+#' @examples
+#' data(mice2)
+#' head(mice2)
+NULL
