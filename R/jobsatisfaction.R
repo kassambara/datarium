e3159493686d96ef3033cd58a8beee6aec3d6e4d
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-03-25 18:24:39 +0100

    New dataset jobsatisfaction added for two-way anova

---
+++ R/jobsatisfaction.R
@@ -0,0 +1,12 @@
+#'Job Satisfaction Data for Two-Way ANOVA)
+#'
+#'@description
+#'  Contains the job satisfaction score organized by gender and education level.
+#'@name jobsatisfaction
+#'@docType data
+#'@usage data("jobsatisfaction")
+#'@format A data frame with 58 rows and 3 columns.
+#' @examples
+#' data(jobsatisfaction)
+#' head(as.data.frame(jobsatisfaction))
+NULL
