ae8f9944fd9c39f1333f12f06b4e864c6f9da230
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2019-10-06 09:21:09 +0200

    New data taskachievment for cochran's q test

---
+++ R/taskachievment.R
@@ -0,0 +1,14 @@
+#'Task Achievment
+#'
+#'@description Repeated measures nominal designs where 73 subjects are asked to
+#'  perform 3 tasks. The outcome of each task is a dichotomous value, success or
+#'  failure. Each row correspond to a participant (called "block" in the
+#'  jargon). This a demo dataset for Cochran's Q test.
+#'@name taskachievment
+#'@docType data
+#'@usage data("taskachievment")
+#'@format A data frame with 73 rows and 4 columns.
+#' @examples
+#' data(taskachievment)
+#' head(taskachievment)
+NULL
